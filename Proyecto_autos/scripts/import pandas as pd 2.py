9096464254057b87f45efa2a0ba81bcba7d2813d
Seba-air85 <se.aird@duocuc.cl>
2026-06-11 18:28:50 -0400

    Delete Proyecto_autos directory

--- Proyecto_autos/scripts/import pandas as pd 2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("dataset_final.csv")
-
-print(df.shape)
-print(df.columns.tolist())
--- Proyecto_autos/scripts/import pandas as pd.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("car details v4.csv")
-
-print(df.columns.tolist())
-print(df.head())
